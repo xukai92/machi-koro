8e1792816f5bf7c94b0878fa80f3dea9b3da3f43
Kai Xu <xukai921110@gmail.com>
2017-12-30 01:45:13 +0000

    Finish tut

--- src/App.js
+++ src/App.js
@@ -1,18 +1,106 @@
-import Client from 'boardgame.io/client';
-import Game from 'boardgame.io/game';
 
-const TicTacToe = Game({
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import BoardgameIO from 'boardgame.io';
+
+function IsVictory(cells) {
+  const positions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let pos of positions) {
+    const symbol = cells[pos[0]];
+    let winner = symbol;
+    for (let i of pos) {
+      if (cells[i] !== symbol) {
+        winner = null;
+        break;
+      }
+    }
+    if (winner != null) return true;
+  }
+
+  return false;
+}
+
+const TicTacToe = BoardgameIO.Game({
   setup: () => ({ cells: Array(9).fill(null) }),
-
+  
   moves: {
     clickCell(G, ctx, id) {
-      let cells = [...G.cells];  // don't mutate original state.
-      cells[id] = ctx.currentPlayer;
-      return { ...G, cells };      // don't mutate original state.
+      const cells = [...G.cells];
+
+      if (cells[id] === null) {
+        cells[id] = ctx.currentPlayer;
+      }
+
+      return { ...G, cells };
     }
-  }
+  },
+  
+  victory: (G, ctx) => IsVictory(G.cells) ? ctx.currentPlayer : null
 });
 
-const App = Client({ game: TicTacToe });
+
+class TicTacToeBoard extends React.Component {
+  onClick(id) {
+    if (this.isActive(id)) {
+      this.props.moves.clickCell(id);
+      this.props.endTurn();
+    }
+  }
+
+  isActive(id) {
+    return this.props.isActive && this.props.G.cells[id] == null;
+  }
+
+  render() {
+    let tbody = [];
+    for (let i = 0; i < 3; i++) {
+      let cells = [];
+      for (let j = 0; j < 3; j++) {
+        const id = 3 * i + j;
+        cells.push(
+          <td key={id}
+              className={this.isActive(id) ? 'active' : ''}
+              onClick={() => this.onClick(id)}>
+            {this.props.G.cells[id]}
+          </td>
+        );
+      }
+      tbody.push(<tr key={i}>{cells}</tr>);
+    }
+
+    let winner = '';
+    if (this.props.ctx.winner !== null) {
+      winner = <div id='winner'>Winner: {this.props.ctx.winner}</div>;
+    }
+
+    return (
+      <div>
+        <table id="board">
+        <tbody>{tbody}</tbody>
+        </table>
+        {winner}
+      </div>
+    );
+  }
+}
+
+var App = BoardgameIO.Client({
+  board: TicTacToeBoard,
+  game: TicTacToe,
+});
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 export default App;
